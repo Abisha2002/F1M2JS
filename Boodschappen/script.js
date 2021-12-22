5fb8ab05e44a4777a9584d10dc46083aedb70d39
abisha <33662@ma-web.nl>
2021-12-22 21:50:46 +0100

    boodschappenlijst button gemaakt

--- Boodschappen/script.js
+++ Boodschappen/script.js
@@ -1,1 +1,12 @@
 console.log("script is geladen");
+
+const groentevak = document.getElementById('groente');
+const groentevak = document.getElementById('fruit');
+
+function zetInFruitLa(){
+    console.log('Zet in fruit la');
+}
+
+function zetInGroenteLa(){
+    console.log('Zet in groenten la');
+}
